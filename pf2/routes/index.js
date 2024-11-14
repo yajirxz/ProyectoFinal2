49e80adfa52e0e148cdeaef6ee906b61690f9ce5
Ing Yahir Turing Automata Boolean <yahirrodriguez647@aragon.unam.mx>
2024-11-14 16:27:24 -0600

    detalleLIbro

--- pf2/routes/index.js
+++ pf2/routes/index.js
@@ -18,6 +18,7 @@ router.get('/libros', function (req, res, next){
  
     }
    // res.json(results);
+   router.get('/libros')
    res.render( 'libros',  {libros: results});
   });
 });
@@ -25,4 +26,46 @@ router.get('/libros', function (req, res, next){
 router.get('/formLibro',function(req,res,next){
   res.render('formulario')
 })
+
+
+router.post('/nuevoLibro',(req, res, next) => {
+  console.log(req.body);
+  //const titulo = req.body.titulo;
+//  const autor = req.body.autor;
+  const {titulo, autor} = req.body;
+  const sql = "INSERT INTO libros (titulo, autor) VALUES(? , ?)";
+
+  db.query(sql, [titulo, autor], (err, result) => {
+    if(err){
+      console.error("Error al guardar libro en base de datos", err);
+      return res.status(500).send("Error al guardar");
+    }else{
+      console.log(result.titulo)
+      res.render('creado', {nuevo : {
+        id : result.insertId,
+        titulo: titulo,
+        autor: autor
+      }
+
+      });
+    }
+  })
+
+});
+
+router.get('/detalleLibro',  (req, res, next) =>{
+  // Recuperar datos de la vista localhost/:3000/detalleLibro?id=7
+  const libroId = req.query.id;
+
+  sql = "SELECT * FROM libros WHERE id = " + libroId;
+  db.query(sql,(error, resultado) =>{
+    if (error){
+      //Mnadar pag personalizada de error
+      console.log(error);
+    }else{
+      res.render('detalle', {libro : resultado[0]})
+    }
+  })
+})
+
 module.exports = router;
